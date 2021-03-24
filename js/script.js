7e3e22ad11e4eabb560f8ecf894c8667618d718c
Mauro Ippoliti <marro1777luv@gmail.com>
2021-03-24 16:35:10 +0100

    push

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+var nuovoCognome = prompt("inserisci il tuo cognome");
+nuovoCognome = nuovoCognome.charAt(0).toUpperCase() + nuovoCognome.slice(1).toLowerCase();
+cognomi.push(nuovoCognome);
+
+var cognomiOrdinati = cognomi.sort();
+console.log(cognomiOrdinati);
+
+// console.log(cognomi.indexOf(nuovoCognome) + 1);
+
+var posizioneUmana;
+
+for (i = 0; i < cognomiOrdinati.length; i++) {
+  if (nuovoCognome == cognomiOrdinati[i]) {
+    posizioneUmana= ++i;
+    console.log(posizioneUmana);
+  }
+}
